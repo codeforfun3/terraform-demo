a7ab7847918462b3b553df0066606adad8a19a97
quocanhkhuong <anh.khuong269@gmail.com>
2024-11-28 23:22:23 -0600

    add tf

---
+++ new_bucket.tf
@@ -0,0 +1,25 @@
+# Specify the provider and access details
+
+provider "aws" {
+  
+}
+
+# Create an S3 bucket
+resource "aws_s3_bucket" "udabucket" {
+  bucket = "cicd-terraform-demo-bucket202k5"
+
+  tags = {
+    Name        = "CICD test bucket"
+    Environment = "Dev"
+  }
+}
+
+# Create an EC2 instance
+resource "aws_instance" "ec2_instance" {
+  ami           = "ami-005fc0f236362e99f"
+  instance_type = "t2.micro"
+
+  tags = {
+    Name = "CICD test instance"
+  }
+}
